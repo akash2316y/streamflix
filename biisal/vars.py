f8db36fcbeb85b612bafa659e60515e577481b51
akash2316y <iamakashrabha@gmail.com>
2026-09-11 23:58:29 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -37,7 +37,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'elated-emlynn-streamfllix-f4229293.koyeb.app')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'streamfllix.koyeb.app')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
